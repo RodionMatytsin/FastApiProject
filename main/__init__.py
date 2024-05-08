d52d59fd7375a2509d2c43f4796bad8ff9ca2963
Rodion Matytsin <79132260269@rambler.ru>
2024-05-08 12:06:06 +0700

    Add views.py file in directory main.

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI(title="Online service on FastAPI")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
+from main.views import views
 from main.api import api_auth
 from main.api import api_cart
 from main.api import api_order
---
+++ main/views/views.py
@@ -0,0 +1,28 @@
+import time
+from main import app, templates
+from fastapi import Request, status
+from fastapi.exceptions import ValidationException
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import HTMLResponse, JSONResponse
+
+
+@app.exception_handler(ValidationException)
+async def validation_exeption_handler(request: Request, exc: ValidationException):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({'detail': exc.errors()})
+    )
+
+
+@app.middleware("http")
+async def before_request(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", context={"request": request})
