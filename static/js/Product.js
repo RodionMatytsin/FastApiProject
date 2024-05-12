1e0c17176a9afdf1a0be104065d94b555534ede9
Rodion Matytsin <79132260269@rambler.ru>
2024-05-12 17:41:48 +0700

    Changes Product.js file.

--- static/js/Product.js
+++ static/js/Product.js
@@ -1,11 +1,12 @@
+const table = document.getElementById('products-table');
+
 function fetchData(url) {
     return fetch(url).then(response => response.json());
 }
 
 function displayProductsTable(data) {
-    const table = document.getElementById('products-table');
     table.innerHTML = '<tbody><tr><td>ID продукта</td><td>Название продукта</td></tr></tbody>';
-    if (data && data.data) {
+    if (data && Array.isArray(data.data)) {
         data.data.forEach(product => {
             const row = table.insertRow();
             row.insertCell(0).textContent = product.product_id;
@@ -23,4 +24,28 @@ document.addEventListener('DOMContentLoaded', () => {
             'Authorization': 'Basic ' + btoa('username:password')
         }
     }).then(displayProductsTable).catch(error => console.error('Ошибка при получении данных:', error));
+
+    document.getElementById('add-product-form').addEventListener('submit', function (event){
+        event.preventDefault();
+        const formCreateProductData = new FormData(this);
+        const name_product = formCreateProductData.get("name_product");
+        fetch('/api/products', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa('username:password'),
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"name_product": name_product})
+        }).then(response => response.json()).then(data => {
+            if (data.result === true) {
+                table.innerHTML = '';
+                fetchData('/api/products').then(displayProductsTable);
+            }
+        }).catch(error => console.error('Ошибка при добавление данных:', error));
+    });
+
+    document.getElementById('clearButton').addEventListener('click', function () {
+        const inputField = document.querySelector('input[name="name_product"]');
+        inputField.value = '';
+    });
 });
