d8c19ff525b0fb1c6a5bb586d61db560a019b46c
Rodion Matytsin <79132260269@rambler.ru>
2024-05-17 22:47:07 +0700

    Changes api_auth.py and api_user.py files

--- main/api/api_user.py
+++ main/api/api_user.py
@@ -1,18 +1,15 @@
 from main import app
 from main.schemas.response import ResponseList
 from main.utils.auth import get_users, get_users_token
-from main.utils.auth import get_us, get_us_token
 
 
 @app.get("/api/users", response_model=ResponseList, tags=["user"])
 async def read_users():
-    # data = await get_users()
-    data = await get_us()
+    data = await get_users()
     return {"result": True, "message": "Успешно, список пользователей был просмотрен!", "data": data}
 
 
 @app.get("/api/users_token", response_model=ResponseList, tags=["user"])
 async def read_users_token():
-    # data = await get_users_token()
-    data = await get_us_token()
+    data = await get_users_token()
     return {"result": True, "message": "Успешно, список токенов был просмотрен!", "data": data}
--- main/utils/auth.py
+++ main/utils/auth.py
@@ -3,12 +3,11 @@ from datetime import datetime, timedelta
 from fastapi import Request
 from main.schemas.user import UserRegular
 from main.models.database import query_execute
-from main.models.fake_db import listUsers, listToken
 from fastapi import HTTPException
 from main.utils.another import escape
 
 
-async def get_u(username: str, password: str) -> UserRegular:
+async def get_user(username: str, password: str) -> UserRegular:
     user = await query_execute(
         query_text=f'SELECT '
                    f'U.id, '
@@ -33,15 +32,7 @@ async def get_u(username: str, password: str) -> UserRegular:
     )
 
 
-# async def get_user(username: str, password: str) -> dict:
-#     for user in listUsers:
-#         if user["username"] == username and user["password"] == password:
-#             return user
-#     detail = {"result": False, "message": "Ошибка, неверный логин или пароль!", "data": {}}
-#     raise HTTPException(status_code=404, detail=detail)
-
-
-async def update_u_t(user_id: int):
+async def update_user_token(user_id: int):
     check_token = await query_execute(
         query_text=f'SELECT * FROM "Tokens" AS T WHERE T.user_id = {user_id}',
         fetch_all=False,
@@ -76,27 +67,7 @@ async def update_u_t(user_id: int):
             return check_token.access_token
 
 
-# async def update_user_token(user_id: int) -> dict:
-#     check_token = next((token for token in listToken if token["user_id"] == user_id), None)
-#     if check_token is None:
-#         new_token = {
-#             "user_id": user_id,
-#             "access_token": str(uuid4()),
-#             "expire": datetime.now() + timedelta(weeks=1),
-#             "datetime_create": datetime.now()
-#         }
-#         listToken.append(new_token)
-#         return new_token
-#     else:
-#         if datetime.now() > check_token["expire"]:
-#             check_token["access_token"] = str(uuid4())
-#             check_token["expire"] = datetime.now() + timedelta(weeks=1)
-#             return check_token
-#         else:
-#             return check_token
-
-
-async def get_u_by_username(username: str) -> UserRegular | None:
+async def get_user_by_username(username: str) -> UserRegular | None:
     user = await query_execute(
         query_text=f'SELECT '
                    f'U.id, '
@@ -118,7 +89,7 @@ async def get_u_by_username(username: str) -> UserRegular | None:
     return None
 
 
-async def get_u_by_email(email: str) -> UserRegular | None:
+async def get_user_by_email(email: str) -> UserRegular | None:
     user = await query_execute(
         query_text=f'SELECT '
                    f'U.id, '
@@ -140,12 +111,12 @@ async def get_u_by_email(email: str) -> UserRegular | None:
     return None
 
 
-async def create_new_u(username: str, password: str, email: str):
-    if await get_u_by_username(username=username):
+async def create_new_user(username: str, password: str, email: str):
+    if await get_user_by_username(username=username):
         detail = {"result": False, "message": "Ошибка, это имя уже занято!", "data": {}}
         raise HTTPException(status_code=409, detail=detail)
 
-    if await get_u_by_email(email=email):
+    if await get_user_by_email(email=email):
         detail = {"result": False, "message": "Ошибка, эта почта уже занята!", "data": {}}
         raise HTTPException(status_code=409, detail=detail)
 
@@ -161,34 +132,11 @@ async def create_new_u(username: str, password: str, email: str):
         raise HTTPException(status_code=500, detail=detail)
 
 
-# async def get_user_by_username(username: str) -> dict:
-#     return next((user for user in listUsers if user["username"] == username), None)
-#
-#
-# async def get_user_by_email(email: str) -> dict:
-#     return next((user for user in listUsers if user["email"] == email), None)
-#
-#
-# async def create_new_user(username: str, password: str, email: str) -> dict:
-#     if await get_user_by_username(username=username):
-#         detail = {"result": False, "message": "Ошибка, это имя уже занято!", "data": {}}
-#         raise HTTPException(status_code=409, detail=detail)
-#
-#     if await get_user_by_email(email=email):
-#         detail = {"result": False, "message": "Ошибка, эта почта уже занята!", "data": {}}
-#         raise HTTPException(status_code=409, detail=detail)
-#
-#     last_user_id = max([user["user_id"] for user in listUsers]) + 1 if listUsers else 0
-#     new_user = {"user_id": last_user_id, "username": username, "email": email, "password": password}
-#     listUsers.append(new_user)
-#     return new_user
-
-
 async def get_user_token(request: Request) -> str:
     return request.cookies.get("user_token")
 
 
-async def get_check_t(access_token: str) -> int | None:
+async def get_check_token(access_token: str) -> int | None:
     user_id = await query_execute(
         query_text=f'SELECT '
                    f'T.id '
@@ -202,17 +150,12 @@ async def get_check_t(access_token: str) -> int | None:
     return None
 
 
-# async def get_check_token(access_token: str) -> int:
-#     return next((token["user_id"] for token in listToken
-#                  if token["access_token"] == access_token and token["expire"] > datetime.now()), None)
-
-
-async def auth_user(request: Request) -> bool:
+async def authenticate_user(request: Request) -> bool:
     token = await get_user_token(request=request)
     if not token:
         detail = {"result": False, "message": "Пожалуйста, войдите в систему!", "data": {}}
         raise HTTPException(status_code=401, detail=detail)
-    check_token = await get_check_t(access_token=token)
+    check_token = await get_check_token(access_token=token)
     if check_token:
         return True
     else:
@@ -220,55 +163,36 @@ async def auth_user(request: Request) -> bool:
         raise HTTPException(status_code=401, detail=detail)
 
 
-# async def authenticate_user(request: Request) -> bool:
-#     token = await get_user_token(request=request)
-#     if not token:
-#         detail = {"result": False, "message": "Пожалуйста, войдите в систему!", "data": {}}
-#         raise HTTPException(status_code=401, detail=detail)
-#     check_token = await get_check_token(access_token=token)
-#     if check_token:
-#         return True
-#     else:
-#         detail = {"result": False, "message": "Ваш токен истек, войдите в систему!", "data": {}}
-#         raise HTTPException(status_code=401, detail=detail)
+async def get_user_id(request: Request) -> int:
+    return await get_check_token(await get_user_token(request))
 
 
-# async def get_user_id(request: Request) -> int:
-#     return await get_check_token(await get_user_token(request))
-
-
-async def get_u_id(request: Request) -> int:
-    return await get_check_t(await get_user_token(request))
-
-
-# async def get_users() -> list:
-#     return sorted(listUsers, key=lambda x: x["user_id"])
-
-
-async def get_us():
-    return await query_execute(
-        query_text='SELECT * FROM "Users" AS U ORDER BY U.id',
+async def get_users():
+    data = await query_execute(
+        query_text=f'SELECT '
+                   f'U.id, '
+                   f'U.username, '
+                   f'U.password, '
+                   f'U.email '
+                   f'FROM "Users" AS U '
+                   f'ORDER BY U.id',
         fetch_all=True,
         type_query='read'
     )
+    return [dict(id=row[0], username=row[1], password=row[2], email=row[3]) for row in data]
 
 
-# async def get_users_token() -> list:
-#     data = [
-#         {
-#             "user_id": token["user_id"],
-#             "access_token": token["access_token"],
-#             "expire": token["expire"].isoformat(),
-#             "datetime_create": token["datetime_create"].isoformat()
-#         }
-#         for token in sorted(listToken, key=lambda x: x["user_id"])
-#     ]
-#     return data
-
-
-async def get_us_token():
-    return await query_execute(
-        query_text='SELECT * FROM "Tokens" AS T ORDER BY T.id',
+async def get_users_token():
+    data = await query_execute(
+        query_text=f'SELECT '
+                   f'T.id, '
+                   f'T.access_token, '
+                   f'T.datetime_create, '
+                   f'T.expires, '
+                   f'T.user_id '
+                   f'FROM "Tokens" AS T '
+                   f'ORDER BY T.id',
         fetch_all=True,
         type_query='read'
     )
+    return [dict(id=i[0], access_token=i[1], datetime_create=i[2], expires=i[3], user_id=i[4]) for i in data]
