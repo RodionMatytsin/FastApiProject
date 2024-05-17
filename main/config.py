2b588a4ab11507590bfdcd4d99308c0672427f00
Rodion Matytsin <79132260269@rambler.ru>
2024-05-17 11:15:29 +0700

    Change port in config.py file

--- main/config.py
+++ main/config.py
@@ -3,5 +3,5 @@ import os
 DATABASE_USER = 'postgres' if os.getenv('DATABASE_USER') is None else os.getenv('DATABASE_USER')
 DATABASE_PASSWORD = 'root' if os.getenv('DATABASE_PASSWORD') is None else os.getenv('DATABASE_PASSWORD')
 DATABASE_IP = 'localhost' if os.getenv('DATABASE_IP') is None else os.getenv('DATABASE_IP')
-DATABASE_PORT = 5454 if os.getenv('DATABASE_PORT') is None else os.getenv('DATABASE_PORT')
+DATABASE_PORT = 5432 if os.getenv('DATABASE_PORT') is None else os.getenv('DATABASE_PORT')
 DATABASE_NAME = 'FastAPI' if os.getenv('DATABASE_NAME') is None else os.getenv('DATABASE_NAME')
