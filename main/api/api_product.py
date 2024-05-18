ad4ec406e8ae20cff26692cd59e85a7b7774eb58
Rodion Matytsin <79132260269@rambler.ru>
2024-05-18 10:41:41 +0700

    Add method get_product in product.py file

--- main/api/api_product.py
+++ main/api/api_product.py
@@ -3,8 +3,7 @@ from fastapi import Request
 from main.schemas.response import ResponseList, ResponseDict
 from main.schemas.product import CreateProductSchema
 from main.utils.auth import authenticate_user
-from main.utils.product import get_products, create_new_product
-# from main.utils.product import get_products, create_new_product, get_product
+from main.utils.product import get_products, create_new_product, get_product
 
 
 @app.get("/api/products", response_model=ResponseList, tags=["products"])
@@ -24,5 +23,5 @@ async def create_product(product: CreateProductSchema, request: Request):
 @app.get("/api/products/{product_id}", response_model=ResponseDict, tags=["products"])
 async def read_product(product_id: int, request: Request):
     await authenticate_user(request)
-    # data = await get_product(product_id=product_id)
-    # return {"result": True, "message": "Успешно, товар был просмотрен!", "data": data}
+    data = await get_product(product_id=product_id)
+    return {"result": True, "message": "Успешно, товар был просмотрен!", "data": data}
--- main/utils/product.py
+++ main/utils/product.py
@@ -19,16 +19,24 @@ async def get_products():
 async def create_new_product(product: str):
     new_product = await query_execute(
         query_text=f'INSERT INTO "Products" (name_product) '
-                   f'VALUES (\'{product}\')',
+                   f'VALUES (\'{escape(product)}\')',
         fetch_all=False,
         type_query='insert'
     )
     return new_product
 
 
-# async def get_product(product_id: int) -> dict:
-#     product = next((product for product in listProducts if product["product_id"] == product_id), None)
-#     if product is None:
-#         detail = {"result": False, "message": "Ошибка, товар с таким идентификатором не найден!", "data": {}}
-#         raise HTTPException(status_code=404, detail=detail)
-#     return product
+async def get_product(product_id: int):
+    product = await query_execute(
+        query_text=f'SELECT '
+                   f'P.id, '
+                   f'P.name_product '
+                   f'FROM "Products" AS P '
+                   f'WHERE P.id = {product_id}',
+        fetch_all=False,
+        type_query='read'
+    )
+    if product is None:
+        detail = {"result": False, "message": "Ошибка, товар с таким идентификатором не найден!", "data": {}}
+        raise HTTPException(status_code=404, detail=detail)
+    return product
